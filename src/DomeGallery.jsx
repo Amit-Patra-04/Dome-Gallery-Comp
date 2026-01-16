4e5b2485b5a5ccf10d27b3d3e1a03f7ebd7303b3
Amit-Patra-04 <k2amitpatra@gmail.com>
2026-01-16 13:45:41 +0530

    Updates

--- src/DomeGallery.jsx
+++ src/DomeGallery.jsx
@@ -11,11 +11,11 @@ const DEFAULT_IMAGES = [
     alt: 'Modern sculpture'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755497595318-7e5e3523854f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'Images/D3.jpg',
     alt: 'Digital artwork'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755353985163-c2a0fe5ac3d8?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'Images/D4.jpg',
     alt: 'Contemporary art'
   },
   {
